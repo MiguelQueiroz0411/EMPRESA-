e3d2d627fae7ccae452de8c21c201337eddf60ac
Pedro <pedro.manoel201599@gmail.com>
2026-09-19 17:28:18 -0300

    criando o menu

--- main.py
+++ main.py
@@ -1,50 +1,31 @@
-from time import sleep
+from cadastro_produtos import cadastrar_produto
+from cadastro_cliente import cadastrar_cliente
+import os
+from colorama import Fore, Style, init
+init()
 
-print('--' * 30)
-print('BEM VINDO A EMPRESA +')
-print("você está na interface de cadastro, tenha cuidado!")
-print('-=' * 10)
 
-opcao = 0
+def menu():
+    while True:
+        try:
+            print(Fore.LIGHTGREEN_EX + "\n=-=-=-=-=-=-=-=MENU=-=-=-=-=-=-=-=")
+            print(Fore.LIGHTYELLOW_EX + "\n1 - Cadastro Produto" + Style.RESET_ALL)
+            print(Fore.LIGHTCYAN_EX + "2 - Cadastro Cliente" + Style.RESET_ALL)
+            print(Fore.LIGHTGREEN_EX + "3 - Sair" + Style.RESET_ALL)
+            opcao = int(input(Fore.LIGHTBLUE_EX + "\nEscolha uma opção: " + Style.RESET_ALL))
+            if opcao == 1:
+                cadastrar_produto()
+                
+            elif opcao == 2:
+                cadastrar_cliente()
+                
+            elif opcao == 3:
+                break
+            
+            else:
+                print(Fore.LIGHTRED_EX + "Opção invalida" + Style.RESET_ALL)
 
-while opcao != 3:
-    print('''[1] Cadastrar cliente
-[2] Cadastrar produto
-[3] Sair''')
+        except ValueError:
+            print(Fore.LIGHTRED_EX + "Valor invalido" + Style.RESET_ALL)
 
-    entrada = input('>>>>> selecione uma opção: ').strip()
-
-    # Valida se a entrada contém apenas dígitos
-    if not entrada.isdigit():
-        print('Erro: A entrada contém caracteres inválidos. Digite apenas números (1, 2 ou 3).')
-        print('-=' * 10)
-        sleep(1.5)
-        continue
-
-    opcao = int(entrada)
-
-    if opcao == 1:
-        cadastro_cliente = str(input('Digite o nome do cliente que sera cadastrado: ')).strip()
-        telefone = str(input('Digite o número do cliente que está sendo cadastrado: ')).strip()
-        email = str(input('Insira o email do cliente que está sendo cadastrado: ')).strip()
-
-        with open("clientes.txt", "a", encoding="utf-8") as arquivo_clientes:
-            arquivo_clientes.write(f"Nome: {cadastro_cliente} | Tel: {telefone} | Email: {email}\n")
-        print(f'Cliente {cadastro_cliente} cadastrado com sucesso.')
-
-    elif opcao == 2:
-        cadastro_produto = str(input('Digite o nome do Produto que você quer cadastrar: ')).strip()
-
-        with open("produtos.txt", "a", encoding="utf-8") as arquivo_produtos:
-            arquivo_produtos.write(f"Produto: {cadastro_produto}\n")
-        print('Produto cadastrado com sucesso.')
-
-    elif opcao == 3:    
-        print('FINALIZANDO...')
-    else:
-        print('Opção inválida: Digite apenas os números correspondentes às opções (1, 2 ou 3).')
-
-    print('-=' * 10)
-    sleep(1.5)
-
-print('FIM DO CADASTRO')
+menu()
